28cfec8a3df512e43a86b5740c1c2a9119587599
Yurippe <yurichikawa1992@gmail.com>
2022-03-14 18:13:55 -0400

    add ability to show first question with a button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,82 @@
+var startEl = document.querySelector("#start-btn");
+
 var quizQuestions = [
     {
         question: "Commonly used data types DO Not Include:",
         answers: {
-            1: "strings",
-            2: "booleans",
-            3: "alerts",
-            4: "numbers",
+            a: "strings",
+            b: "booleans",
+            c: "alerts",
+            d: "numbers",
         }
     },
     {
         question: "The condition in an if / else statement is enclosed with ________.",
         answers: {
-            1: "quotes",
-            2: "curly brackets",
-            3: "parenthesis",
-            4: "square brackets",
+            a: "quotes",
+            b: "curly brackets",
+            c: "parenthesis",
+            d: "square brackets",
         }
     },
     {
         question: "Arrays in JavaScript can be used to store ________.",
         answers: {
-            1: "numbers and strings",
-            2: "other arrays",
-            3: "booleans",
-            4: "all of the above",
+            a: "numbers and strings",
+            b: "other arrays",
+            c: "booleans",
+            d: "all of the above",
         }
     },
     {
         question: "String values must be enclosed within ________ when being assigned to variables.",
         answers: {
-            1: "commas",
-            2: "curly brackets",
-            3: "quotes",
-            4: "parenthesis",
+            a: "commas",
+            b: "curly brackets",
+            c: "quotes",
+            d: "parenthesis",
         }
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: {
-            1: "JavaScript",
-            2: "terminal/bash",
-            3: "for loops",
-            4: "console.log",
+            a: "JavaScript",
+            b: "terminal/bash",
+            c: "for loops",
+            d: "console.log",
         }
     },
 ]
+
+// console.log(quizQuestions[0]);
+// console.log(quizQuestions[0].answers);
+
+var startQuiz = function() {
+    var instruction = document.querySelector("#instruction");
+
+    instruction.remove();
+    startEl.remove();
+
+    switchQuestion(0);
+}
+
+var switchQuestion = function(i) {
+    var sectionArea = document.querySelector("#section-area");
+    
+    var titleArea = document.querySelector("#title-area");
+    titleArea.innerHTML = quizQuestions[i].question;
+
+    var answerListEl = document.createElement("ol");
+    answerListEl.className = "answer-list";
+
+    for (choice in quizQuestions[i].answers) {
+        var listItemEl = document.createElement("li");
+        listItemEl.innerHTML = "<button class='answer'>" + quizQuestions[0].answers[choice] + "</button>";
+        answerListEl.appendChild(listItemEl);
+        console.log(listItemEl);
+    }
+
+    sectionArea.appendChild(answerListEl);
+}
+
+startEl.addEventListener("click", startQuiz);
