74447b011eb32ec544c98b70acad138c189f4ba9
Yurippe <yurichikawa1992@gmail.com>
2022-03-14 16:17:53 -0400

    store questions in array

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+var quizQuestions = [
+    {
+        question: "Commonly used data types DO Not Include:",
+        answers: {
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers",
+        }
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with ________.",
+        answers: {
+            1: "quotes",
+            2: "curly brackets",
+            3: "parenthesis",
+            4: "square brackets",
+        }
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ________.",
+        answers: {
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above",
+        }
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables.",
+        answers: {
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parenthesis",
+        }
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            1: "JavaScript",
+            2: "terminal/bash",
+            3: "for loops",
+            4: "console.log",
+        }
+    },
+]
